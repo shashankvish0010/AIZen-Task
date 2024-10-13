a337fe6ce763f4214c821e01da0677e0bf4e2af3
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-10-14 03:54:03 +0530

    Added Gallery in Dahsboard

--- frontend/aizentask/src/pages/Dashboard.tsx
+++ frontend/aizentask/src/pages/Dashboard.tsx
@@ -21,6 +21,8 @@ const Dashboard: React.FC = () => {
     <div className="min-h-[100vh] w-screen p-3 flex flex-col items-center gap-5">
       <AiImageGenerator />
       <div className="md:w-[85vw] w-[80vw] flex flex-wrap items-center justify-evenly gap-3 p-3">
+        <p className="title text-xl font-semibold">Your Gallery</p>
+        <span className="h-[0.20rem] w-[100%] bg-orange-400 rounded-full"></span>
         {fileHandle?.images && fileHandle.images.length > 0
           ? fileHandle.images.map((image) => (
               <ImageContainer
